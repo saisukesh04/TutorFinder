9c166c23f7eb6bcff9632a10611791112f8b051d
Sai Sukesh <sukesh.sai04@gmail.com>
2020-10-11 10:55:13 +0530

    FeedFragment

--- app/src/main/java/com/vithack/tutorfinder/ui/NewsFeedFragment.java
+++ app/src/main/java/com/vithack/tutorfinder/ui/NewsFeedFragment.java
@@ -4,18 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.vithack.tutorfinder.R;
 
+import butterknife.BindView;
+
 public class NewsFeedFragment extends Fragment {
+
+    @BindView(R.id.feed_recycler) RecyclerView feed_recycler;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_news_feed, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+
         return root;
     }
 }
